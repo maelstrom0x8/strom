831df0142f0076218a82705b78d0a4b6ee6ecf39
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-09 09:55:38 +0100

    Initial commit

---
+++ src/main/java/com/aeflheim/strom/StromApplication.java
@@ -0,0 +1,13 @@
+package com.aeflheim.strom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StromApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeflheim/strom/StromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeflheim.strom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
